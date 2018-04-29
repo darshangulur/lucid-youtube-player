2f4347c565e462d10cb2f9d17fb56cd6ba0e2688
Darshan Gulur Srinivasa <darshan.srinivasa@accesscfa.com>
2018-04-29 16:23:14 -0400

    Tried request to fetch list of videos in playlist. Integrated YTPlayerView.

--- LucidYouTubePlayer/LucidYouTubePlayer/ViewController.swift
+++ LucidYouTubePlayer/LucidYouTubePlayer/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import Alamofire
+import youtube_ios_player_helper
 
 class ViewController: UIViewController {
+    private var playerView = YTPlayerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var pathString = "https://www.googleapis.com/youtube/v3/playlistItems"
+        pathString.append("?part=snippet")
+        pathString.append("&playlistId=PLOU2XLYxmsILwYAhwWBBdDEytdnE0d2-E")
+        pathString.append("&maxResults=50")
+        pathString.append("&key=AIzaSyDBK7Rf8Kup64cWymKwMZeAEOS_x_G0gCw")
+        Alamofire.request(pathString).responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+
+        self.view.addSubview(playerView)
+        playerView.frame = view.frame
+
+        if playerView.load(withVideoId: "IalXtkOdlKg") {
+            playerView.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: YTPlayerViewDelegate {
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+        playerView.playVideo()
+    }
 }
 
